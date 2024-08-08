3f89311d99642b134ea70f4046a6d62b0cae02e0
Andrei Dodu <andrei.dodu@hotmail.it>
2024-08-08 06:36:15 +0200

    refactor: removed useless annotations

--- src/test/java/com/andreidodu/andreitest/cucumber/CucumberConfigurator.java
+++ src/test/java/com/andreidodu/andreitest/cucumber/CucumberConfigurator.java
@@ -7,10 +7,6 @@ import io.cucumber.testng.CucumberOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @CucumberContextConfiguration
-@CucumberOptions(
-        plugin = {"pretty", "json:target/cucumber.json"},
-        glue = {CucumberConfigurationValues.GLUE, CucumberConfigurationValues.CUCUMBER_CONFIG},
-        features = CucumberConfigurationValues.FEATURES)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberConfigurator {
 }
